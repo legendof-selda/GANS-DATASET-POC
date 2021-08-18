5c979f81b384cd130aea47c10fd31a317b06a344
Devicharith <devicharith12@example.com>
2021-08-18 09:49:15 +0000

    Added Final model tfversion 1.15.2

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -13,6 +13,7 @@ import random
 import itertools
 import colorsys
 
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from skimage.measure import find_contours
 import matplotlib.pyplot as plt
@@ -69,7 +70,7 @@ def random_colors(N, bright=True):
     return colors
 
 
-def apply_mask(image, mask, color, alpha=0.5):
+def apply_mask(image, mask, color, alpha=1):
     """Apply the given mask to the image.
     """
     for c in range(3):
@@ -78,6 +79,114 @@ def apply_mask(image, mask, color, alpha=0.5):
                                   (1 - alpha) + alpha * color[c] * 255,
                                   image[:, :, c])
     return image
+
+def save_image(image, image_name, boxes, masks, class_ids, scores, class_names, filter_classs_names=None,
+               scores_thresh=0.1, save_dir=None, mode=0):
+    """
+        image: image array
+        image_name: image name
+        boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
+        masks: [num_instances, height, width]
+        class_ids: [num_instances]
+        scores: confidence scores for each box
+        class_names: list of class names of the dataset
+        filter_classs_names: (optional) list of class names we want to draw
+        scores_thresh: (optional) threshold of confidence scores
+        save_dir: (optional) the path to store image
+        mode: (optional) select the result which you want
+                mode = 0 , save image with bbox,class_name,score and mask;
+                mode = 1 , save image with bbox,class_name and score;
+                mode = 2 , save image with class_name,score and mask;
+                mode = 3 , save mask with black background;
+    """
+    mode_list = [0, 1, 2, 3]
+    assert mode in mode_list, "mode's value should in mode_list %s" % str(mode_list)
+
+    if save_dir is None:
+        save_dir = os.path.join(os.getcwd(), "output")
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+    colors = []
+    ids = []
+    useful_mask_indices = []
+
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances in image %s to draw *** \n" % (image_name))
+        return
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+
+    for i in range(N):
+        # filter
+        class_id = class_ids[i]
+        score = scores[i] if scores is not None else None
+        if score is None or score < scores_thresh:
+            continue
+
+        label = class_names[class_id]
+        if (filter_classs_names is not None) and (label not in filter_classs_names):
+            continue
+
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+
+        useful_mask_indices.append(i)
+        ids.append(label)
+
+    if len(useful_mask_indices) == 0:
+        print("\n*** No instances in image %s to draw *** \n" % (image_name))
+        return
+    
+    for label in ids: 
+      if label=="building":
+        colors.append((219.0,2.0,2.0))
+      if label=="tree":
+        colors.append((2.0,191.0,71.0))
+      if label=="road":
+        colors.append((173.0,176.0,4.0))
+      if label=="grass":
+        colors.append((222.0,127.0,2.0))
+      if label=="flag":
+        colors.append((138.0,2.0,222.0))
+      if label=="car":
+        colors.append((2.0,160.0,222.0))
+
+    if mode != 3:
+        masked_image = image.astype(np.uint8).copy()
+    else:
+        masked_image = np.zeros(image.shape).astype(np.uint8)
+
+    if mode != 1:
+        for index, value in enumerate(useful_mask_indices):
+            masked_image = apply_mask(masked_image, masks[:, :, value], colors[index])
+
+    masked_image = Image.fromarray(masked_image)
+
+    if mode == 3:
+        masked_image.save("/content/drive/MyDrive/Segmentation/GANS-DATASET-POC/results/image.jpg")
+        return
+
+    draw = ImageDraw.Draw(masked_image)
+    colors = np.array(colors).astype(int) * 255
+
+    for index, value in enumerate(useful_mask_indices):
+        class_id = class_ids[value]
+        score = scores[value]
+        label = class_names[class_id]
+
+        y1, x1, y2, x2 = boxes[value]
+        if mode != 2:
+            color = tuple(colors[index])
+            draw.rectangle((x1, y1, x2, y2), outline=color)
+
+        # Label
+        font = ImageFont.load_default()
+        draw.text((x1, y1), "%s %f" % (label, score), (255, 255, 255), font)
+
+    masked_image.save("/content/drive/MyDrive/Segmentation/GANS-DATASET-POC/results/image.jpg")
 
 
 def display_instances(image, boxes, masks, class_ids, class_names,
@@ -174,6 +283,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
+    plt.savefig("/content/drive/MyDrive/Segmentation/GANS-DATASET-POC/results/image.jpg",bbox_inches='tight',pad_inches=0.0)
     if auto_show:
         plt.show()
 
